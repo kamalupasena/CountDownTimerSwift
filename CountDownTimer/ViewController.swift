ac7e54f54cee10a76d17f02212f6c55005537631
kamalupasena <upasenas@gmail.com>
2017-09-13 18:27:16 +0530

    Initial Commit

---
+++ CountDownTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CountDownTimer
+//
+//  Created by Kamal Sampath Upasena on 9/13/17.
+//  Copyright © 2017 Kamal Sampath Upasena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
